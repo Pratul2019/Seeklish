ad04ff8b5c69bf7c001622b0f3e7e410fa4bc438
Pratul Aggarwal <pratulaggarwal1188@gmail.com>
2024-10-09 14:00:06 -0400

    tem

--- next.config.mjs
+++ next.config.mjs
@@ -33,10 +33,6 @@ const nextConfig = {
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
           },
           {
-            key: "Access-Control-Allow-Headers",
-            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-          {
             key: "X-Frame-Options",
             value: "SAMEORIGIN",
           },
